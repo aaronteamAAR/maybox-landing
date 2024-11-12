5a5a878dd7d60c685ae7f6f779175318a2a11106
aaronteamAAR <rooneychris83@gmail.com>
2024-11-12 10:41:44 +0100

    fixed about image

--- src/components/Aboutbody.js
+++ src/components/Aboutbody.js
@@ -9,13 +9,13 @@ const Aboutbody = () => {
           <img
             src={heroimg}
             alt="Hero image"
-            className="w-full hero h-full object-cover rounded-0 md:rounded-3xl"
+            className="w-full hero h-full object-cover rounded-[14px] md:rounded-3xl"
           />
-          <div className="absolute inset-0 bg-black rounded-0 md:rounded-3xl bg-opacity-30 " /> {/* Optional overlay for better text visibility */}
+          <div className="absolute inset-0 rounded-0 md:rounded-3xl bg-opacity-30 " /> 
 
           {/* Added a div with opacity to darken and give the background image some aesthetics and for the div in front of it, emphasis */}
 
-          <div className="absolute bottom-10 left-4 md:bottom-28 md:left-8 z-10 md:w-full w-96 bg-white p-4 md:p-8 max-w-md">
+          <div className="absolute bottom-10 left-4 md:bottom-28 md:left-8 z-10 md:w-full w-96 bg-[#f2efe9] p-4 md:p-8 max-w-md">
             <p className="font-normal pl-[4px] tracking-tight text-[15px] font-custom2 text-customOrangeDark">
               Unbox Your Glow Up
             </p>
@@ -33,7 +33,7 @@ const Aboutbody = () => {
       </section>
 
       <section className="flex justify-center mt-24 font-custom2 px-6 md:px-12">
-        <div className="max-w-3xl">
+        <div className="w-full">
           <p className="mb-4">
             At MayBox, we started with a simple goal: to make beauty better for
             you.{" "}
